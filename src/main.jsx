07c20c7a68471e15cd83ea4d0bba35b1c870c5a3
robonexx <robertwagar@gmail.com>
2023-02-19 08:58:12 +0100

    a little cleaning

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-/* import { createBrowserRouter, RouterProvider } from 'react-router-dom' */
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.scss'
 
-/* const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-
-  }
-]) */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    {/*  <RouterProvider router={router} /> */}
   </React.StrictMode>,
 )
